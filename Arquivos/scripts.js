8174731b239dec6eb847545e3c3c7e9324f6cdb9
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2021-06-23 10:56:53 -0300

    feat: Adicionando Modals na page de cadastro; view: correções no CSS

---
+++ Arquivos/scripts.js
@@ -0,0 +1,14 @@
+function openModal(mn) {
+    let modal = document.getElementById(mn);
+    if (typeof modal == 'undefined' || modal === null)
+        return;
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+}
+function closeModal(mn) {
+    let modal = document.getElementById(mn);
+    if (typeof modal == 'undefined' || modal === null)
+        return;
+    modal.style.display = 'none';
+
+}
