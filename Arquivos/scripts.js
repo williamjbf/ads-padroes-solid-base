63dbd12b05febadcd8e3b965a7acdb44e1307dde
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2021-06-23 21:43:07 -0300

    feat: Adicionando validations na page de cadastro

--- Arquivos/scripts.js
+++ Arquivos/scripts.js
@@ -12,3 +12,31 @@ function closeModal(mn) {
     modal.style.display = 'none';
 
 }
+let nome = document.getElementById('nome');
+let nameValidation = document.getElementById('name-validation');
+nome.onkeyup = function(){
+if(nome.value.length < 3){
+    nameValidation.innerText = 'Por favor, preencha o campo nome';
+    nome.style.border = '';
+    }
+    else{
+        nameValidation.innerText = ''; 
+    }
+}
+let email = document.getElementById('email');
+let mailValidation = document.getElementById('mail-validation');
+email.onkeyup = function(){
+
+    const regexmail =/\S+@\S+\.\S+/;
+    if(regexmail.test(email.value)){
+        mailValidation.innerText = 'Seu email é válido';
+        mailValidation.style.color = 'lime';
+        
+    }
+    else{
+        mailValidation.innerText = 'Seu email não válido';
+        mailValidation.style.color = 'red';
+    }
+
+
+}
