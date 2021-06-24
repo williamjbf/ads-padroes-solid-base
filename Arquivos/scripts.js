61fcee88d10573115f2e0a81839de95a9690c34c
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2021-06-23 21:55:40 -0300

    feat: Adicionando validations na page de cadastro

--- Arquivos/scripts.js
+++ Arquivos/scripts.js
@@ -14,39 +14,39 @@ function closeModal(mn) {
 let operacao = 'A';
 let usuarios;
 
-if (localStorage.getItem('usuarios')){
+if (localStorage.getItem('usuarios')) {
     usuarios = JSON.parse(localStorage.getItem('usuarios'))
 }
-else{
+else {
     usuarios = [];
 }
 
 let indice_selecionado = -1;
 const frmCadastro = document.getElementById('frmCadastro');
 
-function add(){
-    if(operacao == 'A'){
+function add() {
+    if (operacao == 'A') {
         return Adicionar();
     }
-    else{
+    else {
         return Editar();
-        
+
     }
 
 }
 
 function Adicionar() {
-    let usuario ={
+    let usuario = {
         nome: document.querySelector('#txtNome').value,
         email: document.querySelector('#txtEmail').value
     }
     usuarios.push(usuario);
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
-    
+
     return true;
 }
 
-function handleEditar(e){
+function handleEditar(e) {
     operacao = "E";
     indice_selecionado = parseInt(e.getAttribute("alt"));
     let users = JSON.parse(localStorage.getItem('usuarios'));
@@ -61,45 +61,14 @@ function handleEditar(e){
 
 }
 
-<<<<<<< HEAD
-}
-let nome = document.getElementById('nome');
-let nameValidation = document.getElementById('name-validation');
-nome.onkeyup = function(){
-if(nome.value.length < 3){
-    nameValidation.innerText = 'Por favor, preencha o campo nome';
-    nome.style.border = '';
-    }
-    else{
-        nameValidation.innerText = ''; 
-    }
-}
-let email = document.getElementById('email');
-let mailValidation = document.getElementById('mail-validation');
-email.onkeyup = function(){
-
-    const regexmail =/\S+@\S+\.\S+/;
-    if(regexmail.test(email.value)){
-        mailValidation.innerText = 'Seu email é válido';
-        mailValidation.style.color = 'lime';
-        
-    }
-    else{
-        mailValidation.innerText = 'Seu email não válido';
-        mailValidation.style.color = 'red';
-    }
-
-
-}
-=======
-function Editar(){
+function Editar() {
     let users = JSON.parse(localStorage.getItem('usuarios'));
 
     const userAtualizado = {
         nome: document.querySelector("#txtNome").value,
         email: document.querySelector("#txtEmail").value
     }
-    
+
     users[indice_selecionado] = userAtualizado;
     localStorage.setItem('usuarios', JSON.stringify(users));
     alert('Usuario atualizado');
@@ -109,7 +78,7 @@ function Editar(){
 }
 
 
-function listar(){
+function listar() {
     let tbody = document.querySelector('#tblListar tbody');
     let linhas = '';
     let users = JSON.parse(localStorage.getItem('usuarios'));
@@ -129,42 +98,69 @@ function listar(){
     tbody.innerHTML = linhas;
 
 }
-document.querySelector('#modal-cadastrados').addEventListener('click', function(e){
+document.querySelector('#modal-cadastrados').addEventListener('click', function (e) {
     listar();
 })
 
-function deletar(indice){
-    usuarios.splice(indice,1);
-    if(usuarios.length == 0){
+function deletar(indice) {
+    usuarios.splice(indice, 1);
+    if (usuarios.length == 0) {
         localStorage.removeItem('usuarios');
         return;
     }
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
 }
 
-function handleDeletar(e){
+function handleDeletar(e) {
     let indice_selecionado = parseInt(e.getAttribute("alt"));
     deletar(indice_selecionado);
     listar();
 }
 
-function sorteio(){
+function sorteio() {
     usuarios = JSON.parse(localStorage.getItem('usuarios'))
     let max = usuarios.length
     let resultado = [];
     let num = 0;
-    for (let i = 0; i < usuarios.length; i++){
-        num = Math.floor((Math.random() *max) + 0);
-        if(resultado.includes(num)){
+    for (let i = 0; i < usuarios.length; i++) {
+        num = Math.floor((Math.random() * max) + 0);
+        if (resultado.includes(num)) {
             i--;
         }
-        else{
+        else {
             resultado.push(num);
         }
     }
     alert(resultado)
 }
-document.querySelector('#btnSortear').addEventListener('click', function(e){
+document.querySelector('#btnSortear').addEventListener('click', function (e) {
     sorteio()
- })
->>>>>>> 500992fd9f3fd234c60a401dfb3a6e3e9f374c4b
+})
+let nome = document.getElementById('txtNome');
+let nameValidation = document.getElementById('name-validation');
+nome.onkeyup = function () {
+    if (nome.value.length < 3) {
+        nameValidation.innerText = 'Por favor, preencha o campo nome';
+        nome.style.border = '';
+    }
+    else {
+        nameValidation.innerText = '';
+    }
+}
+let email = document.getElementById('txtEmail');
+let mailValidation = document.getElementById('mail-validation');
+email.onkeyup = function () {
+
+    const regexmail = /\S+@\S+\.\S+/;
+    if (regexmail.test(email.value)) {
+        mailValidation.innerText = 'Seu email é válido';
+        mailValidation.style.color = 'lime';
+
+    }
+    else {
+        mailValidation.innerText = 'Seu email não válido';
+        mailValidation.style.color = 'red';
+    }
+}
+
+
